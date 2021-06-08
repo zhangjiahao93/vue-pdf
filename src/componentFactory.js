593f60dde97354e610610ad841533b2f630ff506
zjh <695427431@qq.com>
2021-06-08 09:55:37 +0800

    add mult api

--- src/componentFactory.js
+++ src/componentFactory.js
@@ -1,111 +1,110 @@
-import resizeSensor from 'vue-resize-sensor'
+import resizeSensor from "vue-resize-sensor";
 
-export default function(pdfjsWrapper) {
+export default function (pdfjsWrapper) {
+  var createLoadingTask = pdfjsWrapper.createLoadingTask;
+  var PDFJSWrapper = pdfjsWrapper.PDFJSWrapper;
 
-	var createLoadingTask = pdfjsWrapper.createLoadingTask;
-	var PDFJSWrapper = pdfjsWrapper.PDFJSWrapper;
+  return {
+    createLoadingTask: createLoadingTask,
+    render: function (h) {
+      return h(
+        "span",
+        {
+          attrs: {
+            style: "position: relative; display: block",
+          },
+        },
+        [
+          h("canvas", {
+            attrs: {
+              style: "display: inline-block; width: 100%; height: 100%; vertical-align: top",
+            },
+            ref: "canvas",
+          }),
+          h("span", {
+            style: "display: inline-block; width: 100%; height: 100%",
+            class: "annotationLayer",
+            ref: "annotationLayer",
+          }),
+          h(resizeSensor, {
+            props: {
+              initial: true,
+            },
+            on: {
+              resize: this.resize,
+            },
+          }),
+        ]
+      );
+    },
+    props: {
+      src: {
+        type: [String, Object, Uint8Array],
+        default: "",
+      },
+      page: {
+        type: Number,
+        default: 1,
+      },
+      rotate: {
+        type: Number,
+      },
+      mult: {
+        type: Number,
+        default: 1,
+      },
+    },
+    watch: {
+      src: function () {
+        this.pdf.loadDocument(this.src);
+      },
+      page: function () {
+        this.pdf.loadPage(this.page, this.rotate, this.mult);
+      },
+      rotate: function () {
+        this.pdf.renderPage(this.rotate, this.mult);
+      },
+      mult: function () {
+        this.pdf.renderPage(this.rotate, this.mult);
+      },
+    },
+    methods: {
+      resize: function (size) {
+        // check if the element is attached to the dom tree || resizeSensor being destroyed
+        if (this.$el.parentNode === null || (size.width === 0 && size.height === 0)) return;
 
-	return {
-		createLoadingTask: createLoadingTask,
-		render: function(h) {
-			return h('span', {
-				attrs: {
-					style: 'position: relative; display: block'
-				}
-			}, [
-				h('canvas', {
-					attrs: {
-						style: 'display: inline-block; width: 100%; height: 100%; vertical-align: top',
-					},
-					ref:'canvas'
-				}),
-				h('span', {
-					style: 'display: inline-block; width: 100%; height: 100%',
-					class: 'annotationLayer',
-					ref:'annotationLayer'
-				}),
-				h(resizeSensor, {
-					props: {
-						initial: true
-					},
-					on: {
-						resize: this.resize
-					},
-				})
-			])
-		},
-		props: {
-			src: {
-				type: [String, Object, Uint8Array],
-				default: '',
-			},
-			page: {
-				type: Number,
-				default: 1,
-			},
-			rotate: {
-				type: Number,
-			},
-		},
-		watch: {
-			src: function() {
+        // on IE10- canvas height must be set
+        this.$refs.canvas.style.height = this.$refs.canvas.offsetWidth * (this.$refs.canvas.height / this.$refs.canvas.width) + "px";
+        // update the page when the resolution is too poor
+        var resolutionScale = this.pdf.getResolutionScale();
 
-				this.pdf.loadDocument(this.src);
-			},
-			page: function() {
+        if (resolutionScale < 0.85 || resolutionScale > 1.15) this.pdf.renderPage(this.rotate, this.multiply);
 
-				this.pdf.loadPage(this.page, this.rotate);
-			},
-			rotate: function() {
-				this.pdf.renderPage(this.rotate);
-			},
-		},
-		methods: {
-			resize: function(size) {
+        // this.$refs.annotationLayer.style.transform = 'scale('+resolutionScale+')';
+      },
+      print: function (dpi, pageList) {
+        this.pdf.printPage(dpi, pageList);
+      },
+    },
 
-				// check if the element is attached to the dom tree || resizeSensor being destroyed
-				if ( this.$el.parentNode === null || (size.width === 0 && size.height === 0) )
-					return;
+    // doc: mounted hook is not called during server-side rendering.
+    mounted: function () {
+      this.pdf = new PDFJSWrapper(this.$refs.canvas, this.$refs.annotationLayer, this.$emit.bind(this));
 
-				// on IE10- canvas height must be set
-				this.$refs.canvas.style.height = this.$refs.canvas.offsetWidth * (this.$refs.canvas.height / this.$refs.canvas.width) + 'px';
-				// update the page when the resolution is too poor
-				var resolutionScale = this.pdf.getResolutionScale();
+      this.$on("loaded", function () {
+        this.pdf.loadPage(this.page, this.rotate, this.mult);
+      });
 
-				if ( resolutionScale < 0.85 || resolutionScale > 1.15 )
-					this.pdf.renderPage(this.rotate);
+      this.$on("page-size", function (width, height) {
+        this.$refs.canvas.style.height = this.$refs.canvas.offsetWidth * (height / width) + "px";
+      });
 
-				// this.$refs.annotationLayer.style.transform = 'scale('+resolutionScale+')';
-			},
-			print: function(dpi, pageList) {
+      this.pdf.loadDocument(this.src);
+    },
 
-				this.pdf.printPage(dpi, pageList);
-			}
-		},
-
-		// doc: mounted hook is not called during server-side rendering.
-		mounted: function() {
-
-			this.pdf = new PDFJSWrapper(this.$refs.canvas, this.$refs.annotationLayer, this.$emit.bind(this));
-
-			this.$on('loaded', function() {
-
-				this.pdf.loadPage(this.page, this.rotate);
-			});
-
-			this.$on('page-size', function(width, height) {
-
-				this.$refs.canvas.style.height = this.$refs.canvas.offsetWidth * (height / width) + 'px';
-			});
-
-			this.pdf.loadDocument(this.src);
-		},
-
-		// doc: destroyed hook is not called during server-side rendering.
-		destroyed: function() {
-
-			this.pdf.destroy();
-		}
-	}
-
+    // doc: destroyed hook is not called during server-side rendering.
+    destroyed: function () {
+      this.pdf.destroy();
+    },
+  };
 }
--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -1,24 +1,20 @@
 <style src="./annotationLayer.css"></style>
 <script>
+  import componentFactory from "./componentFactory.js";
+  var component;
+  if (process.env.VUE_ENV !== "server") {
+    var pdfjsWrapper = require("./pdfjsWrapper.js").default;
+    var PDFJS = require("pdfjs-dist/es5/build/pdf.js");
 
-	import componentFactory from './componentFactory.js'
+    if (typeof window !== "undefined" && "Worker" in window && navigator.appVersion.indexOf("MSIE 10") === -1) {
+      var PdfjsWorker = require("worker-loader!pdfjs-dist/es5/build/pdf.worker.js");
+      PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+    }
 
-	if ( process.env.VUE_ENV !== 'server' ) {
+    component = componentFactory(pdfjsWrapper(PDFJS));
+  } else {
+    component = componentFactory({});
+  }
 
-		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
-		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
-
-		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
-
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
-			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
-		}
-
-		var component = componentFactory(pdfjsWrapper(PDFJS));
-	} else {
-
-		var component = componentFactory({});
-	}
-
-	export default component;
+  export default component;
 </script>
